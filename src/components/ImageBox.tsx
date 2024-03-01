96e39fc1fe2ee6e9e30667ec0440c48a229be63c
r0sen-ritter <safinm007@gmail.com>
2024-03-01 16:09:45 +0600

    boundary scale

--- src/components/ImageBox.tsx
+++ src/components/ImageBox.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import "./ImageBox.css";
 import ContentImage from "/pittsburgh.jpg";
 import Point from "./Point";
@@ -17,16 +17,13 @@ interface ImageBoxProps {
 
 const ImageBox = ({ positions, setPositions, radius }: ImageBoxProps) => {
   const [dragging, setDragging] = useState<string | null>(null);
-  const [pointsSelected, setPointsSelected] = useState<string[]>([]);
+  const [scaling, setScaling] = useState<boolean>(false);
+  const [selectedPoint, setSelectedPoint] = useState<string | null>(null);
 
   const draggingRef = useRef(dragging);
 
-  const handleMouseDown = (
-    id: string,
-    setPointSelected: (bool: boolean) => void,
-    pointSelected: boolean
-  ) => {
-    setPointSelected(!pointSelected);
+  const handleMouseDown = (id: string) => {
+    setSelectedPoint((prevId) => (prevId === id ? null : id));
     setDragging(id);
     window.addEventListener("mouseup", handleMouseUp);
   };
@@ -36,41 +33,113 @@ const ImageBox = ({ positions, setPositions, radius }: ImageBoxProps) => {
     window.removeEventListener("mouseup", handleMouseUp);
   };
 
-  const handleMouseMove = (e: MouseEvent) => {
-    const currentDragging = draggingRef.current;
-    if (currentDragging) {
-      const newPosition = calculatePosition(e, radius);
-      if (newPosition.x !== 0 || newPosition.y !== 0) {
-        setPositions((prev) =>
-          prev.map((point) => {
-            if (Object.keys(point)[0] === currentDragging) {
-              return { [currentDragging]: newPosition };
-            }
-            return point;
-          })
-        );
+  const handleMouseMove = useCallback(
+    (e: MouseEvent) => {
+      const currentDragging = draggingRef.current;
+      if (currentDragging) {
+        const newPosition = calculatePosition(e, radius);
+        if (newPosition.x !== 0 || newPosition.y !== 0) {
+          setPositions((prev) =>
+            prev.map((point) => {
+              if (Object.keys(point)[0] === currentDragging) {
+                return { [currentDragging]: newPosition };
+              }
+              return point;
+            })
+          );
+        }
       }
-    }
-  };
+    },
+    [radius, setPositions]
+  );
 
-  const handleKeyPress = (e: KeyboardEvent) => {
-    console.log(e.key);
-    if (e.key === "Delete") {
-      setPositions((prev) =>
-        prev.filter((point) => !pointsSelected.includes(Object.keys(point)[0]))
-      );
-      setPointsSelected([]);
-    }
-  };
+  const scalePositions = useCallback(
+    (direction: "up" | "down") => {
+      let boundaryFound: boolean = false;
+      setPositions((prev) => {
+        const relativeCenter = prev.reduce(
+          (acc, point) => {
+            const key = Object.keys(point)[0];
+            const { x, y } = point[key];
+            return { x: acc.x + x / prev.length, y: acc.y + y / prev.length };
+          },
+          { x: 0, y: 0 }
+        );
+
+        console.log(boundaryFound);
+
+        const newPositions = prev.map((point) => {
+          const key = Object.keys(point)[0];
+          const { x, y } = point[key];
+
+          const shiftedX = x - relativeCenter.x;
+          const shiftedY = y - relativeCenter.y;
+          const scaledX = shiftedX * (direction === "up" ? 1.1 : 0.9);
+          const scaledY = shiftedY * (direction === "up" ? 1.1 : 0.9);
+          let newX = scaledX + relativeCenter.x;
+          let newY = scaledY + relativeCenter.y;
+
+          if (
+            newX < 0 ||
+            newX > 280 - 2 * radius ||
+            newY < 0 ||
+            newY > 280 - 2 * radius
+          ) {
+            boundaryFound = true;
+          }
+
+          newX = Math.max(0, Math.min(newX, 280 - 2 * radius));
+          newY = Math.max(0, Math.min(newY, 280 - 2 * radius));
+
+          const newPoint = {
+            [key]: {
+              x: newX,
+              y: newY,
+            },
+          };
+          return newPoint;
+        });
+
+        return boundaryFound ? prev : newPositions;
+      });
+    },
+    [setPositions, radius]
+  );
+  const handleKeyPress = useCallback(
+    (e: KeyboardEvent | WheelEvent) => {
+      if ("key" in e) {
+        if (e.key === "Delete" || e.key === "Backspace") {
+          setPositions((prev) =>
+            prev.filter((point) => Object.keys(point)[0] !== selectedPoint)
+          );
+          setSelectedPoint(null);
+        }
+      } else if ("deltaY" in e) {
+        if (e.ctrlKey || e.metaKey) {
+          setScaling(true);
+          e.preventDefault();
+          if (e.deltaY < 0) {
+            scalePositions("up");
+          } else {
+            scalePositions("down");
+          }
+        }
+        setTimeout(() => setScaling(false), 100);
+      }
+    },
+    [selectedPoint, setPositions, setSelectedPoint, scalePositions, setScaling]
+  );
 
   useEffect(() => {
     window.addEventListener("mousemove", handleMouseMove);
-    window.addEventListener("keypress", handleKeyPress);
+    window.addEventListener("keydown", handleKeyPress);
+    window.addEventListener("wheel", handleKeyPress, { passive: false });
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
-      window.removeEventListener("keypress", handleKeyPress);
+      window.removeEventListener("keydown", handleKeyPress);
+      window.removeEventListener("wheel", handleKeyPress);
     };
-  }, []);
+  }, [handleKeyPress, handleMouseMove]);
 
   useEffect(() => {
     draggingRef.current = dragging;
@@ -102,10 +171,14 @@ const ImageBox = ({ positions, setPositions, radius }: ImageBoxProps) => {
   };
 
   return (
-    <div id="image-box">
+    <div id="image-box" className={scaling ? "scaling" : ""}>
       <img id="image" src={ContentImage} />
       {positions.map((point) => (
-        <Point point={point} handleMouseDown={handleMouseDown} />
+        <Point
+          point={point}
+          handleMouseDown={handleMouseDown}
+          isSelected={Object.keys(point)[0] === selectedPoint}
+        />
       ))}
       {!dragging &&
         positions.map((point, index, array) => {
--- src/components/Point.tsx
+++ src/components/Point.tsx
@@ -1,5 +1,4 @@
 import "./Point.css";
-import { useState } from "react";
 
 interface PositionPoint {
   [key: string]: { x: number; y: number };
@@ -7,16 +6,11 @@ interface PositionPoint {
 
 interface PointProps {
   point: PositionPoint;
-  handleMouseDown: (
-    id: string,
-    setPointSelected: (bool: boolean) => void,
-    pointSelected: boolean
-  ) => void;
+  handleMouseDown: (id: string) => void;
+  isSelected: boolean;
 }
 
-const Point = ({ point, handleMouseDown }: PointProps) => {
-  const [pointSelected, setPointSelected] = useState(false);
-
+const Point = ({ point, handleMouseDown, isSelected }: PointProps) => {
   const id = Object.keys(point)[0];
   const { x, y } = point[id];
 
@@ -28,9 +22,9 @@ const Point = ({ point, handleMouseDown }: PointProps) => {
       style={{
         left: `${x}px`,
         top: `${y}px`,
-        borderColor: `${pointSelected ? "green" : "red"}`,
+        borderColor: `${isSelected ? "green" : "red"}`,
       }}
-      onMouseDown={() => handleMouseDown(id, setPointSelected, pointSelected)}
+      onMouseDown={() => handleMouseDown(id)}
     />
   );
 };
