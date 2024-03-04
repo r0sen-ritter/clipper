17a39ae6e9115a2f491b360baad4ec1a43044f57
r0sen-ritter <safinm007@gmail.com>
2024-03-04 16:24:02 +0600

    drag shape

--- src/components/ClipPath.tsx
+++ src/components/ClipPath.tsx
@@ -11,8 +11,8 @@ const ClipPath = ({ positions }: ClipPathProps) => {
   const points = positions
     .map((point) => {
       const key = Object.keys(point)[0];
-      return `${Math.round((point[key].x / 280) * 100)}% ${Math.round(
-        (point[key].y / 280) * 100
+      return `${Math.round((point[key].x / 270) * 100)}% ${Math.round(
+        (point[key].y / 270) * 100
       )}%`;
     })
     .join(", ");
